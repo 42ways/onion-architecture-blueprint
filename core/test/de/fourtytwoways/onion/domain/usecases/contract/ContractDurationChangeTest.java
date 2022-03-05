caafde5e96b46a10ed61da0532bd4c84f5f0fbbd
Thomas Herrmann <mail@thoherr.de>
2022-03-05 23:41:58 +0100

    add duration change tests

---
+++ core/test/de/fourtytwoways/onion/domain/usecases/contract/ContractDurationChangeTest.java
@@ -0,0 +1,44 @@
+package de.fourtytwoways.onion.domain.usecases.contract;
+
+import de.fourtytwoways.onion.domain.entities.contract.Contract;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractDurationChangeTest {
+
+    @Test
+    void adjustStartDate() {
+        Contract contract = new Contract("4711", null,
+                LocalDate.of(2022, 1, 1), LocalDate.of(2042, 1, 1),
+                BigDecimal.valueOf(4711), null);
+        new ContractCalculation().calculatePremium(contract);
+        assertEquals(BigDecimal.valueOf(19.63), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(4711), contract.getBenefit());
+        new ContractDurationChange().adjustStartDate(contract, LocalDate.of(2032, 1, 1), false);
+        assertEquals(BigDecimal.valueOf(39.26), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(4711), contract.getBenefit());
+        new ContractDurationChange().adjustStartDate(contract, LocalDate.of(2022, 1, 1));
+        assertEquals(BigDecimal.valueOf(39.26), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(9422.40), contract.getBenefit().stripTrailingZeros());
+    }
+
+    @Test
+    void adjustEndDate() {
+        Contract contract = new Contract("4711", null,
+                LocalDate.of(2022, 1, 1), LocalDate.of(2042, 1, 1),
+                BigDecimal.valueOf(4711), null);
+        new ContractCalculation().calculatePremium(contract);
+        assertEquals(BigDecimal.valueOf(19.63), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(4711), contract.getBenefit());
+        new ContractDurationChange().adjustEndDate(contract, LocalDate.of(2032, 1, 1));
+        assertEquals(BigDecimal.valueOf(19.63), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(2355.60), contract.getBenefit().stripTrailingZeros());
+        new ContractDurationChange().adjustEndDate(contract, LocalDate.of(2042, 1, 1),false);
+        assertEquals(BigDecimal.valueOf(9.82), contract.getPremium());
+        assertEquals(BigDecimal.valueOf(2355.60), contract.getBenefit().stripTrailingZeros());
+    }
+}
