7d31c4efd2339cd4d2622873ae3856221186c82f
Thomas Herrmann <mail@thoherr.de>
2022-03-06 17:57:45 +0100

    simplify code with lombok; chang toString to lombok conventions (despite case)

--- core/src/de/fourtytwoways/onion/domain/entities/enumeration/AbstractEnumValue.java
+++ core/src/de/fourtytwoways/onion/domain/entities/enumeration/AbstractEnumValue.java
@@ -1,24 +1,16 @@
 package de.fourtytwoways.onion.domain.entities.enumeration;
 // (c) 2022 Thomas Herrmann, 42ways GmbH
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 
-@EqualsAndHashCode
+@Data
 public abstract class AbstractEnumValue implements EnumValue {
     final EnumType type;
-    @Getter final int id;
-    @Getter final String key;
-    @Getter final String value;
-
-    protected AbstractEnumValue(EnumType type, int id, String key, String value) {
-        this.type = type;
-        this.id = id;
-        this.key = key;
-        this.value = value;
-    }
+    final int id;
+    final String key;
+    final String value;
 
     public final String toString() {
-        return type + " [" + id + ", " + key + ", " + value + "]";
+        return type + "(" + id + ", " + key + ", " + value + ")";
     }
 }
