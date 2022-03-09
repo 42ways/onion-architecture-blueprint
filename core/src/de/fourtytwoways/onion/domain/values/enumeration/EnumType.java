883869038b9acd8cc52d893dc4c643ecb48d4357
Thomas Herrmann <mail@thoherr.de>
2022-03-09 22:45:58 +0100

    fix copyright comment

--- core/src/de/fourtytwoways/onion/domain/values/enumeration/EnumType.java
+++ core/src/de/fourtytwoways/onion/domain/values/enumeration/EnumType.java
@@ -1,8 +1,5 @@
 package de.fourtytwoways.onion.domain.values.enumeration;
-
 // Copyright (c) 2022 Thomas Herrmann, 42ways GmbH
-
-// (c) 2022 Thomas Herrmann, 42ways GmbH
 
 public enum EnumType {
     DOCUMENT_TYPE,
