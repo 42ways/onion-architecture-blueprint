c639acd26a4792d5f2953b28921045a22ae6198e
Thomas Herrmann <mail@thoherr.de>
2022-03-13 00:32:31 +0100

    seal example person repository class

--- infrastructure/src/main/java/de/fourtytwoways/onion/infrastructure/people/db/ExamplePersonRepository.java
+++ infrastructure/src/main/java/de/fourtytwoways/onion/infrastructure/people/db/ExamplePersonRepository.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 
-public class ExamplePersonRepository implements PersonRepository {
+public final class ExamplePersonRepository implements PersonRepository {
     private final EnumRepository enumRepository;
 
     public ExamplePersonRepository(EnumRepository enumRepository) {
