308b302ec68af71ec9fedc47e7a539f2185519ef
Thomas Herrmann <mail@thoherr.de>
2022-03-12 12:35:12 +0100

    simplify lambdas, fix BigDecimal handling

--- core/src/test/java/de/fourtytwoways/onion/domain/values/MoneyTest.java
+++ core/src/test/java/de/fourtytwoways/onion/domain/values/MoneyTest.java
@@ -4,6 +4,7 @@ package de.fourtytwoways.onion.domain.values;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +13,14 @@ class MoneyTest {
     @Test
     void valueOf() {
         Money m1 = Money.valueOf(1.0);
-        assertEquals(Money.valueOf(new BigDecimal(1.), Money.Currency.EUR), m1);
+        assertEquals(Money.valueOf(BigDecimal.valueOf(1.), Money.Currency.EUR), m1);
         assertEquals(Money.valueOf(1., Money.Currency.EUR), m1);
         assertEquals(Money.valueOf(1, Money.Currency.EUR), m1);
 
         Money m2 = Money.valueOf(1.0, Money.Currency.USD);
         assertEquals(Money.valueOf(1, Money.Currency.USD), m2);
         assertNotEquals(m1, m2);
-        assertNotEquals(Money.valueOf(new BigDecimal(1.), Money.Currency.EUR), m2);
+        assertNotEquals(Money.valueOf(BigDecimal.valueOf(1.), Money.Currency.EUR), m2);
         assertNotEquals(Money.valueOf(1., Money.Currency.EUR), m2);
         assertNotEquals(Money.valueOf(1, Money.Currency.EUR), m2);
 
@@ -35,9 +36,7 @@ class MoneyTest {
         Money m2 = Money.valueOf(3.4);
         assertEquals(Money.valueOf(4.6), m1.add(m2));
         Money m3 = Money.valueOf(3.4, Money.Currency.USD);
-        assertThrows(IllegalArgumentException.class, () -> {
-            m1.add(m3);
-        });
+        assertThrows(IllegalArgumentException.class, () -> m1.add(m3));
     }
 
     @Test
@@ -46,9 +45,7 @@ class MoneyTest {
         Money m2 = Money.valueOf(3.4);
         assertEquals(Money.valueOf(-2.2), m1.subtract(m2));
         Money m3 = Money.valueOf(3.4, Money.Currency.USD);
-        assertThrows(IllegalArgumentException.class, () -> {
-            m1.subtract(m3);
-        });
+        assertThrows(IllegalArgumentException.class, () -> m1.subtract(m3));
     }
 
     @Test
@@ -57,9 +54,7 @@ class MoneyTest {
         Money m2 = Money.valueOf(3.4);
         assertEquals(Money.valueOf(4.08), m1.multiply(m2));
         Money m3 = Money.valueOf(3.4, Money.Currency.USD);
-        assertThrows(IllegalArgumentException.class, () -> {
-            m1.multiply(m3);
-        });
+        assertThrows(IllegalArgumentException.class, () -> m1.multiply(m3));
     }
 
     @Test
@@ -68,15 +63,13 @@ class MoneyTest {
         Money m2 = Money.valueOf(3.4);
         assertEquals(Money.valueOf(0.35), m1.divide(m2));
         Money m3 = Money.valueOf(3.4, Money.Currency.USD);
-        assertThrows(IllegalArgumentException.class, () -> {
-            m1.divide(m3);
-        });
+        assertThrows(IllegalArgumentException.class, () -> m1.divide(m3));
     }
 
     @Test
     void getAmount() {
         Money m1 = Money.valueOf(1.2);
-        assertEquals(BigDecimal.valueOf(1.2).setScale(2), m1.getAmount());
+        assertEquals(BigDecimal.valueOf(1.2).setScale(2, RoundingMode.HALF_UP), m1.getAmount());
     }
 
     @Test
