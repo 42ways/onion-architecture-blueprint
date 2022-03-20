dfee7686b7940f917303733e460a88f4b40eea3c
Thomas Herrmann <mail@thoherr.de>
2022-03-20 22:24:00 +0100

    add design question

--- core/src/main/java/de/fourtytwoways/onion/domain/entities/document/Document.java
+++ core/src/main/java/de/fourtytwoways/onion/domain/entities/document/Document.java
@@ -6,5 +6,5 @@ import de.fourtytwoways.onion.domain.values.enumeration.DocumentType;
 public interface Document {
     int getId();
     DocumentType getDocumentType();
-    void print();
+    void print(); // TODO: Is that really part of this interface? Do we really care in our core domain?
 }
